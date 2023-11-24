1ae6e1b293bbc442cbbbca39a2533a33c5e62f8b
Silbye <silverxlxl9@gmail.com>
2023-11-24 17:22:57 +0100

    made header and footer as components

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="header-left">
+        <img src="../assets/img/Logo.png" alt="" class="header-logo" />
+      </div>
+      <div class="header-right">
+        <router-link to="/" class="header-link">Home</router-link>
+        <router-link to="/" class="header-link">Project</router-link>
+        <router-link to="/blog" class="header-link">Blog</router-link>
+      </div>
+    </div>
+  </header>
+</template>
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
